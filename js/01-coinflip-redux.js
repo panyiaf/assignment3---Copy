8f56c5501d448054459d8bcf1558f69062e3fc36
Panyia Fang <panyiafang@gmail.com>
2022-07-21 23:40:16 -0700

    init

--- js/01-coinflip-redux.js
+++ js/01-coinflip-redux.js
@@ -1,16 +1,12 @@
-// let coinFlip;
-// let loopTime = Number(prompt('Please insert number of times you\'d like to loop'));
+let coinFlip;
+let loopTime = Number(prompt('Please insert number of times you\'d like to loop'));
 
-// for (let i = 0; i < loopTime; i++) {
-//     coinFlip = Math.round(Math.random());
+for (let i = 0; i < loopTime; i++) {
+    coinFlip = Math.round(Math.random());
 
-//     if(coinFlip === 0) {
-//         console.log('Heads');
-//     } else {
-//         console.log('Tails');
-//     }
-// }
-
-let a = 0;
-for (a; a < 5; a++);
-console.log(a);
+    if(coinFlip === 0) {
+        console.log('Heads');
+    } else {
+        console.log('Tails');
+    }
+}
